0da252c8d474bcd06889817f5d3f28ebbdf46cfa
karlyPuentes <k.puentes@udla.edu.co>
2025-04-24 00:09:54 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import Header from "./components/header/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import { GeneroProvider } from "./context/GeneroContext";
 
 createRoot(document.getElementById("root")).render(
